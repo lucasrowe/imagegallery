b6fe9a59bcfd151ddab601fa5f333047de84db40
Lucas Rowe <lucas.rowe@gmail.com>
2017-03-25 17:34:22 -0400

    Add dummy infinite scrolling

---
+++ imagegallery.js
@@ -0,0 +1,21 @@
+$('.swipeboxExampleImg').swipebox();
+
+$('#mygallery').justifiedGallery({
+    lastRow : 'nojustify',
+    rowHeight : 200,
+    rel : 'gallery2',
+    margins : 1
+}).on('jg.complete', function () {
+    $('.swipebox').swipebox();
+});
+
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+    for (var i = 0; i < 5; i++) {
+      $('#mygallery').append('<a class="gallery swipebox" href="photos/kitten.jpg">' +
+          '<img src="photos/kitten.jpg" />' +
+          '</a>');
+    }
+    $('#mygallery').justifiedGallery('norewind');
+  }
+});
